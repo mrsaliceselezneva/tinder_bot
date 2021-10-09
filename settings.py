fdccfe79b7ef60f0c04703efbb5550cf4e3a4f10
N777 <Egorov_vadim2001@mail.ru>
2021-10-09 12:37:56 +0400

    Delete settings.py

--- settings.py
+++
@@ -1,4 +0,0 @@
-API_KEY = "653738077:AAGgRbasq5zgCDz405Vtr68JLjFK9Z9RbzU"
-EMOJI = [":smile:", ":sunglasses:", ":book:", ":memo:", ":tada:"]
-EMOJI_NUM = [":one:", ":two:", ":three:", ":four:", ":five:",
-             ":six:", ":seven:", ":eight:", ":nine:", ":ten:"]

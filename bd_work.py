4ffe253f81ef583f6b6821b3653211dc8000cd29
IronArt <poluvesov.artyom@gmail.com>
2021-10-09 16:36:07 +0400

    поиск друга и изменение анкеты

---
+++ bd_work.py
@@ -0,0 +1,8 @@
+def bd_edit_anketa(update, contex):  # изменение анкеты
+    user_message = update.message.text
+    return
+
+
+def bd_search(update, contex):  # поиск друга
+    user_message = update.message.text
+    return
